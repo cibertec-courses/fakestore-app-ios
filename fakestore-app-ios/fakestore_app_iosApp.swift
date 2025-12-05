16a1f48e05a13bb50bcf3954aa118bc58e4b6944
Christiam mendoza <ch.alb.rz@hotmail.com>
2025-12-04 21:24:58 -0500

    Inital commit

---
+++ fakestore-app-ios/fakestore_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakestore_app_iosApp.swift
+//  fakestore-app-ios
+//
+//  Created by DamII on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct fakestore_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
